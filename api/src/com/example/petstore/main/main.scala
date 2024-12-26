f1bab209785289e84b75de9b13857e1da4a7633a
Sakib Hadziavdic <sakib@sake.ba>
2024-12-26 16:17:05 +0100

    Add main.scala

---
+++ api/src/com/example/petstore/main/main.scala
@@ -0,0 +1,19 @@
+package com.example.petstore.main
+
+import io.undertow.Undertow
+import ba.sake.sharaf.*, routing.*
+import com.example.petstore.api.controllers.*
+
+@main def apiMain =
+  val allRoutes = Seq[Routes](
+    // uncomment once you generate the sources!
+    /*new UserController().routes,
+    new PetController().routes,
+    new StoreController().routes*/
+  )
+  Undertow.builder
+    .addHttpListener(8080, "localhost")
+    .setHandler(SharafHandler(Routes.merge(allRoutes)))
+    .build
+    .start()
+  println("Server started at http://localhost:8080")
