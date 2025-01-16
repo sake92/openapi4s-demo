3dcd3b61c137eef199dd8f61cb743f929c6c38ad
Sakib Hadziavdic <sakib@sake.ba>
2025-01-16 14:09:05 +0100

    Add http4s example config

--- api/src/com/example/petstore/main/main.scala
+++ api/src/com/example/petstore/main/main.scala
@@ -6,10 +6,9 @@ import com.example.petstore.api.controllers.*
 
 @main def apiMain =
   val allRoutes = Seq[Routes](
-    // uncomment once you generate the sources!
-    /*new UserController().routes,
+    new UserController().routes,
     new PetController().routes,
-    new StoreController().routes*/
+    new StoreController().routes
   )
   Undertow.builder
     .addHttpListener(8080, "localhost")
@@ -17,3 +16,33 @@ import com.example.petstore.api.controllers.*
     .build
     .start()
   println("Server started at http://localhost:8080")
+
+/* http4s version */
+/*
+import cats.effect.{IO, IOApp, Resource}
+import cats.implicits._
+import org.http4s.implicits._
+import com.comcast.ip4s._
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.middleware.Logger
+import ba.sake.petstore.api.routes.*
+
+object Main extends IOApp.Simple {
+  override def run: IO[Unit] = (for {
+    _ <- Resource.pure(())
+    httpApp = (
+      new UserRoutes().routes <+>
+        new PetRoutes().routes <+>
+        new StoreRoutes().routes
+      ).orNotFound
+    finalHttpApp = Logger.httpApp(true, true)(httpApp)
+    _ <-
+      EmberServerBuilder
+        .default[IO]
+        .withHost(ipv4"0.0.0.0")
+        .withPort(port"8080")
+        .withHttpApp(finalHttpApp)
+        .build
+  } yield ()).useForever
+}
+ */
