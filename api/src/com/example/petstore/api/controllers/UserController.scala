6f586e2de6dd9263ddae41c13a0c2c381bc1b69f
Sakib Hadziavdic <sakib@sake.ba>
2025-03-20 11:51:57 +0100

    fmt

--- api/src/com/example/petstore/api/controllers/UserController.scala
+++ api/src/com/example/petstore/api/controllers/UserController.scala
@@ -1,4 +1,5 @@
 package com.example.petstore.api.controllers
+
 import java.time.*
 import java.util.UUID
 import io.undertow.util.StatusCodes
@@ -6,26 +7,39 @@ import ba.sake.querson.QueryStringRW
 import ba.sake.validson.Validator
 import ba.sake.sharaf.*, routing.*
 import com.example.petstore.api.models.*
+
 class UserController {
   def routes = Routes {
     case POST() -> Path("user") =>
       val reqBody = Request.current.bodyJsonValidated[User]
-      Response.withStatus(StatusCodes.NOT_IMPLEMENTED).withBody("TODO: return User")
+      Response
+        .withStatus(StatusCodes.NOT_IMPLEMENTED)
+        .withBody("TODO: return User")
     case POST() -> Path("user", "createWithList") =>
       val reqBody = Request.current.bodyJsonValidated[Seq[User]]
-      Response.withStatus(StatusCodes.NOT_IMPLEMENTED).withBody("TODO: return User")
+      Response
+        .withStatus(StatusCodes.NOT_IMPLEMENTED)
+        .withBody("TODO: return User")
     case GET() -> Path("user", "login") =>
-      case class QP(username: Option[String], password: Option[String]) derives QueryStringRW
+      case class QP(
+        username: Option[String],
+        password: Option[String]
+      ) derives QueryStringRW
       val qp = Request.current.queryParamsValidated[QP]
-      Response.withStatus(StatusCodes.NOT_IMPLEMENTED).withBody("TODO: return String")
+      Response
+        .withStatus(StatusCodes.NOT_IMPLEMENTED)
+        .withBody("TODO: return String")
     case GET() -> Path("user", "logout") =>
       Response.withStatus(StatusCodes.NOT_IMPLEMENTED)
     case GET() -> Path("user", username) =>
-      Response.withStatus(StatusCodes.NOT_IMPLEMENTED).withBody("TODO: return User")
+      Response
+        .withStatus(StatusCodes.NOT_IMPLEMENTED)
+        .withBody("TODO: return User")
     case PUT() -> Path("user", username) =>
       val reqBody = Request.current.bodyJsonValidated[User]
       Response.withStatus(StatusCodes.NOT_IMPLEMENTED)
     case DELETE() -> Path("user", username) =>
       Response.withStatus(StatusCodes.NOT_IMPLEMENTED)
   }
+
 }
--- api/src/com/example/petstore/api/models/Customer.scala
+++ api/src/com/example/petstore/api/models/Customer.scala
@@ -1,7 +1,13 @@
 package com.example.petstore.api.models
+
 import java.time.*
 import java.util.UUID
 import org.typelevel.jawn.ast.JValue
 import ba.sake.tupson.*
 import ba.sake.validson.Validator
-case class Customer(id: Option[Long], username: Option[String], address: Option[Seq[Address]]) derives JsonRW
+
+case class Customer(
+  id: Option[Long],
+  username: Option[String],
+  address: Option[Seq[Address]]
+) derives JsonRW
