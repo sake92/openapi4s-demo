be9c465e95f6801b2f4ec087178f8b4ee85c5fe7
Sakib Hadziavdic <sakib@sake.ba>
2025-03-26 02:05:25 +0100

    Fix openapi-diff.sh

--- scripts/openapi-diff.sh
+++ scripts/openapi-diff.sh
@@ -1,5 +1,5 @@
 
-git show main:api/resources/public/openapi.json > main_openapi.json
+git show origin/main:api/resources/public/openapi.json > main_openapi.json
 
 cs launch org.openapitools.openapidiff:openapi-diff-cli:2.1.0-beta.12 -M org.openapitools.openapidiff.cli.Main -- --fail-on-incompatible main_openapi.json ./api/resources/public/openapi.json
 [ $? -eq 0 ]  || exit 1
